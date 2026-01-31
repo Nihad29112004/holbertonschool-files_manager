9fea9e79341e803668a3b2c6e306aa5c237ceaa3
Nihad ALiyev <lnihad257@gmail.com>
2026-01-31 11:24:56 +0400

    redis

--- utils/redis.mjs
+++ utils/redis.mjs
@@ -4,14 +4,9 @@ import { promisify } from 'util';
 class RedisClient {
   constructor() {
     this.client = createClient();
-
     this.client.on('error', (err) => {
-      console.log(`Redis client not connected to the server: ${err}`);
+      console.log(`Redis client not connected to the server: ${err.message || err}`);
     });
-    // Promisification des méthodes Redis
-    this.getAsync = promisify(this.client.get).bind(this.client);
-    this.setAsync = promisify(this.client.set).bind(this.client);
-    this.delAsync = promisify(this.client.del).bind(this.client);
   }
 
   isAlive() {
@@ -19,30 +14,18 @@ class RedisClient {
   }
 
   async get(key) {
-    try {
-      const value = await this.getAsync(key);
-      return value;
-    } catch (err) {
-      console.error('Error retrieve value:', err);
-      return null;
-    }
+    const getAsync = promisify(this.client.get).bind(this.client);
+    return getAsync(key);
   }
 
   async set(key, value, duration) {
-    try {
-      // this key will expire after the duration explain in seconds
-      await this.setAsync(key, value, 'EX', duration);
-    } catch (err) {
-      console.error('Error setting value:', err);
-    }
+    const setAsync = promisify(this.client.setex).bind(this.client);
+    await setAsync(key, duration, value);
   }
 
   async del(key) {
-    try {
-      await this.delAsync(key);
-    } catch (err) {
-      console.error('Error deleting value:', err);
-    }
+    const delAsync = promisify(this.client.del).bind(this.client);
+    await delAsync(key);
   }
 }
 
